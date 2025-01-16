376f5813bdfe8431b63b5cfb2ffd1d35b32dd74d
Dhay <Diego50094@gmail.com>
2025-01-15 23:46:56 -0300

    Adiciona inicialização zerada e uniforme

--- src/initialization.py
+++ src/initialization.py
@@ -13,3 +13,13 @@ class WeightInitialization(object):
 class GaussianInitialization(WeightInitialization):
     def gen_weights(self, input_dim, output_dim):
         return np.random.normal(loc = 0, scale=sqrt(2/(input_dim+output_dim)), size=(input_dim, output_dim))
+    
+# Inicialização zerada
+class ZeroInitialization(WeightInitialization):
+    def gen_weights(self, input_dim, output_dim):
+        return np.zeros(shape=(input_dim, output_dim), dtype='float64')
+    
+# Inicialização uniforme
+class UniformInitialization(WeightInitialization):
+    def gen_weights(self, input_dim, output_dim):
+        return np.random.uniform(low = -sqrt(6/(input_dim+output_dim)), high=sqrt(6/(input_dim+output_dim)), shape=(input_dim, output_dim), dtype='float64')
