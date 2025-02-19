b6da1d2a5a0d44efd5ee2755a5f625e19d60c4c1
Dhay <Diego50094@gmail.com>
2025-02-18 22:00:41 -0300

    Atualiza loss MSE do módulo src_torch. Remove outras losses

--- src_torch/loss_functions.py
+++ src_torch/loss_functions.py
@@ -8,42 +8,16 @@ class LossFunction(object):
     __metaclass__ = abc.ABCMeta
     
     @abc.abstractmethod
-    def loss(self, prediction: Tensor, expected: Tensor) -> Tensor:
-        return
-    
-    @abc.abstractmethod
-    def loss_der(self, prediction: Tensor, expected: Tensor) -> Tensor:
+    def loss(self, prediction: Tensor, expected: Tensor, device) -> Tensor:
         return
     
 class MSE(LossFunction):
-    def loss(self, prediction: Tensor, expected: Tensor) -> Tensor:
-        loss_val = 0.0
+    def loss(self, prediction: Tensor, expected: Tensor, device) -> Tensor:
+        loss_val = torch.zeros(1, device=device)
         for i in range(len(prediction)):
             l = prediction[i] - expected[i]
             loss_val += 0.5*l*l
         return loss_val
-    
-    def loss_der(self, prediction: Tensor, expected: Tensor) -> Tensor:
-        return prediction - expected
-    
-class MAE(LossFunction):
-    def loss(self, prediction: Tensor, expected: Tensor) -> Tensor:
-        loss_val = 0.0
-        for i in range(len(prediction)):
-            l = abs(prediction[i] - expected[i])
-            loss_val += l
-        return loss_val
-    
-    def loss_der(self, prediction: Tensor, expected: Tensor) -> Tensor:
-        result = []
-        for i in range(len(prediction)):
-            if prediction[i] > expected[i]:
-                result.append(1)
-            elif prediction[i] < expected[i]:
-                result.append(-1)
-            else:
-                result.append(0)
-        return torch.from_numpy(np.array(result))
     
 class CrossEntropy(LossFunction):
     def loss(self, prediction, expected, device):
@@ -53,22 +27,5 @@ class CrossEntropy(LossFunction):
             cross = -(e*torch.log(p)+(torch.tensor(1, device=device)-e)*torch.log(torch.tensor(1, device=device)-p))
             sum += cross 
         return sum
-    
-    def loss_der(self, prediction: Tensor, expected: Tensor):
-        prediction = prediction*torch.tensor(0.99999) + torch.tensor(0.000005)
-        return -(expected/prediction - (1-expected)/(1-prediction))
-    
-class MQE(LossFunction):
-    def loss(self, prediction, expected):
-        loss_val = 0.0
-        for i in range(len(prediction)):
-            l = prediction[i] - expected[i]
-            loss_val += 0.25*l*l*l*l
-        return loss_val
-    
-    def loss_der(self, prediction, expected):
-        pred_sq = prediction*prediction
-        exp_sq = expected*expected
-        return prediction*pred_sq - 3*pred_sq*expected + 3*prediction*exp_sq - expected*exp_sq
     
     
